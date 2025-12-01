383809fc263ae54614e387db530abfabb3204885
IbnoeRizal <ibnurizal849@gmail.com>
2025-12-01 09:49:57 +0700

    add hsvmasking and bw filtering blob to act like Treshold masking

--- guico.py
+++ guico.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from typing import Dict
 import cv2 as cv
 import numpy as np
-from numpy._core.numeric import ndarray
+from numpy._core.numeric import array, ndarray
+from numpy._core.numerictypes import uint8
 
 
 class Img:
@@ -32,6 +34,19 @@ class Img:
                                 [-2,5,-2],
                                 [1,-2,1]])
 
+    HSVMASKCOLORRANGE = {
+        "redDown":  np.array([  0,   0,  14], dtype=np.uint8),   # 0–10
+        "redUP":    np.array([242, 255, 255], dtype=np.uint8),   # 170–179
+        "orange":   np.array([ 14,  28,  35], dtype=np.uint8),   # 10–25
+        "yellow":   np.array([ 35,  49,  63], dtype=np.uint8),   # 25–35
+        "green":    np.array([ 49, 121, 170], dtype=np.uint8),   # 35–85
+        "cyan":     np.array([121, 142, 170], dtype=np.uint8),   # 85–100
+        "blue":     np.array([142, 185, 213], dtype=np.uint8),   # 100–130
+        "purple":   np.array([185, 213, 228], dtype=np.uint8),   # 130–150
+        "magenta":  np.array([213, 234, 242], dtype=np.uint8),   # 150–170
+        "pink":     np.array([228, 242, 255], dtype=np.uint8),   # 160–179 (soft)
+    } 
+
     def __init__(self, p: str | np.ndarray) -> None:
 
         self.img = None
@@ -58,6 +73,16 @@ class Img:
         return any(p.lower().endswith(ext) for ext in Img.supportedExtention)
 
     def toGrayscale(self) -> np.ndarray | None:
+        """
+        mengubah gambar menjadi skala abu
+
+        Weighted formula: Gray = 0.2989*R + 0.5870*G + 0.1140*B 
+
+        return
+        ______
+        image : np.ndarray
+            ndim(2)
+        """
         if self.img is None:
             return None
         elif len(self.img.shape) == 2:
@@ -72,6 +97,19 @@ class Img:
         return np.clip(gray, 0, 255).astype(np.uint8)
 
     def toBW(self, intensity: int = 127) -> np.ndarray | None:
+        """
+        Mengubah image menjadi satu channel dengan treshold pixel 127 (default)
+
+        Parameter
+        _________
+        intensity: int = 25
+            nilai treshold (ambang), pixel = 0 jika nilai pixel < intensity selain itu pixel = 1
+
+        Return
+        ______
+        image : np.ndarray
+            ndim(2)
+        """
         gr = self.toGrayscale()
 
         if gr is None:
@@ -90,11 +128,32 @@ class Img:
         return bw
 
     def toNeg(self) -> np.ndarray | None:
+        """
+        mencerminkan nilai pixel 0 sampai 255
+
+        Return
+        ______
+        image : np.ndarray
+            ndim(3 or 2)
+        """
         if self.img is None:
             return None
         return (255 - self.img).astype(np.uint8)
     
     def dissableexcept(self,select:str) -> np.ndarray | None:
+        """
+        menonaktifkan dua channel rgb kecuali yang dipilih
+        
+        Parameter
+        _________
+        select:str
+            option (r or g or b)
+
+        Return
+        ______
+        image : np.ndarray
+            ndim(3)
+        """
         if self.img is None or self.img.ndim < 3:
             return None
         select = select.strip().lower()
@@ -112,6 +171,14 @@ class Img:
 
     @staticmethod
     def to3Channel(obj: Img) -> bool:
+        """
+        Membuat image dengan ndim = 2 menjadi ndim = 3
+
+        Return
+        ______
+        image : np.ndarray 
+            ndim(3)
+        """
         if obj.img is None or len(obj.img.shape) != 2:
             return False
         obj.img = np.stack((obj.img, obj.img, obj.img), axis=-1)
@@ -123,7 +190,23 @@ class Img:
         return Img(path)
 
     def stretchPixelDist(self, low = 0, high = 255) -> np.ndarray | None:
+        """
+        Menghitung persebaran nilai pixel, dan meregangkannya dari 0 ke 255 (default)
+        
+        Parameter
+        _________
+        low = 0: int
+            default batas kiri peregangan
 
+        high = 255: int
+            default batas kanan peregangan
+
+        Return
+        ______
+        image : np.ndarray 
+            ndim(2 or 3)
+        
+        """
         low = min(255, max(0,low))
         high = min(255, max(0, high))
 
@@ -151,6 +234,23 @@ class Img:
         return temp
 
     def contrastAdjust(self, alpha=1.2) -> np.ndarray | None:
+        """
+        Menaikkan atau Menurunkan nilai pixel terhadap rata - rata tiap channel warna
+
+        Parameter
+        _________
+        alpha: float
+            seberapa besar penambahan jarak dari rata - rata pixel
+            alpha < 0 : nilai pixel bergeser menjauhi ke kiri
+            alpha = 0 : nilai pixel bergeser menjadi rata - rata nilai pixel
+            alpha = 1 : nilai pixel tetap
+            alpha > 1 : menjauhi persen jarak dari rata - rata pixel
+
+        Return
+        ______
+            image : np.ndarray
+                tergantung dimensi input 2d atau 3d
+        """
         if self.img is None:
             return None
         channel:int = self.img.ndim or 0
@@ -172,9 +272,133 @@ class Img:
         return None
 
     def convolv(self,kernelOpt:np.ndarray) -> np.ndarray | None:
+        """
+        Menjalankan konvolusi terhadap gambar dengan kernel
+        
+        Parameter
+        _________
+        kernelOpt : np.ndarray 
+            kernel lowpass atau highpass tersedia sebagai variabel kelas
+
+        Return
+        ______
+        filtered image : np.ndarray
+
+        """
         if self.img is None or kernelOpt is None:
             return None
         return cv.filter2D(self.img,-1,kernelOpt)
+    
+    def clean_binary_mask(self, min_area: int = 500) -> np.ndarray|None:
+        """
+        Membersihkan biner mask:
+        1. Threshold
+        2. Morphology opening (hapus noise kecil)
+        3. Connected component filtering (hapus area kecil sekali)
+
+        Parameter
+        ---------
+        min_area : int
+            Area minimum blob yang dipertahankan.
+
+        Return
+        ------
+        mask : np.ndarray
+            Binary image (0/255) yang bersih.
+        """
+
+        # 1. Threshold → biner 0 / 255
+        mask = self.img if self.img.ndim == 2 else self.toBW()
+        if mask is None: 
+            return None
+
+        # 2. Morphology opening → hapus titik/noise kecil
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+        mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=1)
+
+        # 3. Connected Components → hapus blob kecil
+        num_labels, labels, stats, _ = cv.connectedComponentsWithStats(mask, connectivity=8)
+
+        # stats[:, cv.CC_STAT_AREA] berisi luas setiap area
+        cleaned = np.zeros_like(mask)
+
+        for i in range(1, num_labels):  # abaikan label 0 (background)
+            area = stats[i, cv.CC_STAT_AREA]
+            if area >= min_area:
+                cleaned[labels == i] = 255
+
+        return cleaned
+
+    def hsvMask(self,colorToMask:str) -> np.ndarray | None:
+        """
+        mengambil warna dari image 3 channel, mengembalikan mask fuzzy ranges 0 to 1 terhadap image tersebut 1 channel
+        
+        Parameter
+        _________
+        colorToMask: str
+            the key of Img.HSVMASKCOLORRANGE
+
+        Return
+        ______
+        image: np.ndarray
+            ndim(2) ranges 0.0 to 1.0
+        """
+        colorToMask = colorToMask.strip().lower()
+        if self.img is None or not colorToMask or self.img.ndim != 3:
+            return None
+
+        hsv = cv.cvtColor(self.img,cv.COLOR_BGR2HSV)
+        H = hsv[:,:,0].astype(np.float32) * (255/179) #mengambil channel pertama yaitu Hue normalisasi menjadi 0 sampai 255
+        S = hsv[:,:,1]#mengambil channel kedua yaitu saturation
+        V = hsv[:,:,2]#mengambil channel ketiga yaitu value
+
+       
+        def fuzzyTriangle(x, abc):
+            left, peak, right = abc
+
+            res = np.zeros_like(x, dtype=np.float32)
+
+            # sisi kiri (naik)
+            mask_left = (x >= left) & (x <= peak)
+            res[mask_left] = (x[mask_left] - left) / (peak - left)
+
+            # sisi kanan (turun)
+            mask_right = (x > peak) & (x <= right)
+            res[mask_right] = (right - x[mask_right]) / (right - peak)
+
+            return res
+
+        def fuzzyTrapesium(x, abc, R=True):
+            left, peak, right = abc
+
+            result = np.zeros_like(x, dtype=np.float32)
+
+            if R:
+                left_mask = (x >= left) & (x <= peak)
+                result[left_mask] = (x[left_mask] - left) / (peak - left)
+            else:
+                right_mask = ((x >= peak) & (x <= right))
+                result[right_mask] = (right - x[right_mask]) / (right - peak)
+
+
+            peak_mask = ((x > peak) & (x <= right)) if R else ((x < peak) & (x >= left))
+            result[peak_mask] = 1.0
+
+            return result
+
+        SV = fuzzyTrapesium(S,np.array([30, 50, 255], dtype=np.uint8),R=True) * fuzzyTriangle(V,np.array([10, 120, 230], dtype=np.uint8))
+
+        if colorToMask == "red":
+            L = fuzzyTrapesium(H,self.HSVMASKCOLORRANGE[colorToMask + "UP"],R=True)
+            R = fuzzyTrapesium(H,self.HSVMASKCOLORRANGE[colorToMask + "Down"],R=False)
+            return np.maximum(L,R) 
+
+        if colorToMask in self.HSVMASKCOLORRANGE:
+            return fuzzyTriangle(H, self.HSVMASKCOLORRANGE[colorToMask]) 
+        else: return None
+        
+
+
 
 
 
@@ -182,10 +406,9 @@ if __name__ == "__main__":
     print('this code section is for testing only')
     inp = input("masukkan path").strip()
     mg = Img(inp)
-
-    w = mg.convolv(kernelOpt=Img.kernelHighpass2)
-
+     
+    w = mg.hsvMask("blue") 
     cv.namedWindow("stretchPixelDist", cv.WINDOW_NORMAL)
-    cv.imshow("stretchPixelDist", w)
+    cv.imshow("stretchPixelDist", w )
     cv.waitKey(0)
     cv.destroyAllWindows()
